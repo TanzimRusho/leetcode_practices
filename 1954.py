0e1a6eac45aacb2a8ee9819795c528f3ec5354dd
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-08-01 10:50:09 +0600

    Updated to OOP style

--- 1954.py
+++ 1954.py
@@ -1,7 +1,18 @@
-neededApples = int(input())
-
-for i in range(10000001):
-    sum = 2 * i * (i+1) * (2*i + 1)    
-    if neededApples <= sum:
-        print(8*i)
-        break 
+class Solution:
+    def minimumPerimeter(self, neededApples: int) -> int:
+        for i in range(10000001):
+            sum = 2 * i  * (i + 1) * (2 * i + 1)
+            if sum >= neededApples:
+                return 8*i
+                
+def main():
+    solve = Solution()
+    
+    neededApples = int(input())
+    
+    ans = solve.minimumPerimeter(neededApples)
+    
+    print(ans)
+    
+if __name__ == "__main__":
+    main()
