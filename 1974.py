a4246413c02ecbbc89858bfc4143d5de74133dae
Tanzim Ahmed <32319601+TanzimRusho@users.noreply.github.com>
2021-08-21 22:03:58 +0600

    Add files via upload

---
+++ 1974.py
@@ -0,0 +1,16 @@
+class Solution:
+    def minTimeToType(self, word: str) -> int:
+        moves = 0
+        prev = 'a'
+        
+        for ch in word:
+            dif = abs(ord(ch) - ord(prev))
+            moves += min(dif, 26-dif) + 1
+            prev = ch
+        
+        return moves 
+
+sn = Solution()    
+print(sn.minTimeToType("zjpc"))
+
+
